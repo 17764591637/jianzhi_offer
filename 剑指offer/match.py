9cf6b423fdc6e84ccdf1241eec0ef9884492376c
rps <17764591637@163.com>
2019-07-19 16:29:17 +0800

    first commit

--- 剑指offer/match.py
+++ 剑指offer/match.py
@@ -4,12 +4,30 @@
 匹配是指字符串的所有字符匹配整个模式。例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，
 但是与"aa.a"和"ab*a"均不匹配
 '''
-class Solution:
-    # s, pattern都是字符串
-    def match(self, s, pattern):
-        # write code here
+import re 
+#代码一：
+# class Solution:
+#     # s, pattern都是字符串
+#     def match(self, s, pattern):
+#         # write code here
+#         return True if re.match("^"+pattern+"$",s) else False
         
+#代码二：
+class Solution:
+    def match(self,s,pattern):
+        if (len(s)==0 and len(pattern)==0):
+            return True
+        if (len(s)>0 and len(pattern)==0):
+            return False
+        if (len(pattern)>1 and pattern[1] == '*'):
+            if (len(s)>0 and (s[0]==pattern[0] or pattern[0]=='.')):
+                return (self.match(s, pattern[2:]) or self.match(s[1:], pattern[2:]) or self.match(s[1:], pattern))
+            else:
+                return self.match(s, pattern[2:])
+        if (len(s) > 0 and (pattern[0] == '.' or pattern[0] == s[0])):
+            return self.match(s[1:], pattern[1:])
+        return False
 
 s = Solution()
-res = s.match('werewr','ewrewrw')
+res = s.match('aaa','ab*ac*a')
 print(res)

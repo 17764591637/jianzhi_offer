18c5738fd613f4196b93e46d241c276d1bb35eb7
rps <17764591637@163.com>
2019-07-26 14:00:42 +0800

    first commit

--- 剑指offer/58_isSymmetrical.py
+++ 剑指offer/58_isSymmetrical.py
@@ -1,5 +1,5 @@
 '''
-请实现一个函数，用来判断一颗二叉树是不是对称的。注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+
 '''
 # -*- coding:utf-8 -*-
 class TreeNode:
@@ -7,7 +7,53 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
-class Solution:
-    def isSymmetrical(self, pRoot):
-        # write code here
+#递归
+# class Solution(object):
+#     def isSymmetrical(self, pRoot):
+#         """
+#         :type root: TreeNode
+#         :rtype: bool
+#         """
+#         if pRoot == None:
+#             return True
         
+#         return self.checkTwoTree(pRoot.left,pRoot.right)
+    
+#     def checkTwoTree(self, leftTree, rightTree):
+#         if leftTree==None and rightTree==None:
+#             return True
+#         if leftTree!=None and rightTree==None:
+#             return False
+#         if leftTree==None and rightTree!=None:
+#             return False
+#         if leftTree.val != rightTree.val:
+#             return False
+#         left = self.checkTwoTree(leftTree.left,rightTree.right)
+#         right = self.checkTwoTree(leftTree.right,rightTree.left)
+        
+#         return left and right
+#迭代
+class Solution(object):
+    def isSymmetrical(self, pRoot):
+        if not pRoot:
+            return True
+        node_list = [pRoot.left,pRoot.right]
+        while node_list:
+            symmetricLeft = node_list.pop(0)
+            symmetricRight = node_list.pop(0)
+            if not symmetricLeft and not symmetricRight:
+                continue
+            if not symmetricLeft or not symmetricRight:
+                return False
+            if symmetricLeft.val != symmetricRight.val:
+                return False
+            node_list.append(symmetricLeft.left)
+            node_list.append(symmetricRight.right)
+            node_list.append(symmetricLeft.right)
+            node_list.append(symmetricRight.left)
+            
+        return True
+
+
+
+

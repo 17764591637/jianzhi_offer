9066469fb34cd2531de03cad87e607a75a84e37d
rps <17764591637@163.com>
2019-07-26 15:18:42 +0800

    first commit

--- 剑指offer/39_IsBalanced_Solution.py
+++ 剑指offer/39_IsBalanced_Solution.py
@@ -10,4 +10,20 @@ class TreeNode:
 class Solution:
     def IsBalanced_Solution(self, pRoot):
         # write code here
-        
+        if not pRoot:
+            return True
+
+        left_deepth = self.TreeDepth(pRoot.left)
+        right_deepth = self.TreeDepth(pRoot.right)
+        if abs(left_deepth-right_deepth) <= 1:
+            return True
+        return False
+
+    def TreeDepth(self, pRoot):
+        if not pRoot:
+            return 0
+        nleft = self.TreeDepth(pRoot.left)
+        nright = self.TreeDepth(pRoot.right)
+
+        return max(nleft+1,nright+1)
+

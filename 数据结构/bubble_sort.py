fdcfb0218069796820c2d3bd58ac3a3143b21bf7
rps <17764591637@163.com>
2019-08-01 19:42:45 +0800

    first commit

--- 数据结构/bubble_sort.py
+++
@@ -1,9 +0,0 @@
-def bubble_sort(nums):
-    for i in range(len(nums)-1,0,-1):
-        for j in range(i):
-            if nums[j] > nums[j+1]:
-                nums[j],nums[j+1] = nums[j+1],nums[j]
-
-nums = [2,1,4,6,3]
-bubble_sort(nums)
-print(nums)
--- 数据结构/insert_sort.py
+++
@@ -1,9 +0,0 @@
-def insert_sort(nums):
-    for i in range(1,len(nums)):
-        for j in range(i,0,-1):
-            if nums[j] < nums[j-1]:
-                nums[j], nums[j - 1] = nums[j - 1], nums[j]
-
-alist = [54,26,93,17,77,31,44,55,20]
-insert_sort(alist)
-print(alist)

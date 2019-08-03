1b0c3231b494e877b35c491ea6c182ca135d01e7
rps <17764591637@163.com>
2019-08-03 16:49:57 +0800

    first commit

--- 剑指offer/code_test.py
+++ 剑指offer/code_test.py
@@ -1,11 +1,31 @@
-zhishu_list = {2,7}
-start = 14
-res= []
-for j in zhishu_list:
-    if j not in {2,3,5}:
-        start += 1
-else:
-    res.append(14)
-    start += 1
-    
+n = int(input())
+num_list = list(map(int,input().strip().split()))
+s = ''
+for i in num_list:
+    s += str(i)
+
+def Permutation(n):
+    ss = ''
+    for i in range(1,n+1):
+        ss += str(i)
+    # write code here
+    if ss == "":
+        return []
+    res = []
+    def backtrack(ss,tmp):
+        if not ss and tmp not in res:
+            res.append(tmp)
+            return
+        for i in range(len(ss)):
+            backtrack(ss[:i]+ss[i+1:],tmp+ss[i])
+    backtrack(ss,'')
+    return res 
+
+pailie = Permutation(n)
+index = pailie.index(s)
+pailie.reverse()
+r = list(pailie[index])
+res = ''
+for i in r:
+    res += i + ' '
 print(res)

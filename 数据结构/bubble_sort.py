6b9cd02425b6dd5ab08ad3aae0f5ed72bd21039a
rps <17764591637@163.com>
2019-08-06 11:09:23 +0800

    first commit

--- 数据结构/bubble_sort.py
+++ 数据结构/bubble_sort.py
@@ -1,8 +1,17 @@
+'''
+1.比较相邻的元素。如果第一个比第二个大，就交换它们两个；
+2.对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对，这样在最后的元素应该会是最大的数；
+3.针对所有的元素重复以上的步骤，除了最后一个；
+4.重复步骤1~3，直到排序完成。
+时间复杂度：O(n2)
+稳定
+'''
 def bubble_sort(nums):
     for i in range(len(nums)-1,0,-1):
         for j in range(i):
             if nums[j] > nums[j+1]:
                 nums[j],nums[j+1] = nums[j+1],nums[j]
+        print(nums)
 
 nums = [2,1,4,6,3]
 bubble_sort(nums)

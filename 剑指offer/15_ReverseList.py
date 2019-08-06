c3cc63701670c13836b2a616c0ea23948139f471
rps <17764591637@163.com>
2019-08-06 15:50:21 +0800

    first commit

--- 剑指offer/15_ReverseList.py
+++ 剑指offer/15_ReverseList.py
@@ -1,11 +1,17 @@
 '''
 输入一个链表，反转链表后，输出新链表的表头。
+
+思路：
+step1:None
+step2:1->None 
+step3:2->1->None
+... 
 '''
 class Solution:
     # 返回ListNode
     def ReverseList(self, pHead):
         # write code here
-        if  not pHead or not pHead.next:
+        if not pHead or not pHead.next:
             return pHead
         p_before = None 
         pNode = pHead

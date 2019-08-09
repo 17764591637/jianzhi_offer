c1e3cb507687cb3062e657f42156cab91bb8bdb9
rps <17764591637@163.com>
2019-08-09 10:44:28 +0800

    first commit

--- 剑指offer/26_Convert.py
+++ 剑指offer/26_Convert.py
@@ -3,11 +3,11 @@
 要求不能创建任何新的结点，只能调整树中结点指针的指向。
 '''
 # -*- coding:utf-8 -*-
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 class Solution:
     def Convert(self, pRootOfTree):
         # write code here

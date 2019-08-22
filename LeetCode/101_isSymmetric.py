98d6b3ac82786570ff839f9e8f2fa874937e4454
rps <17764591637@163.com>
2019-08-22 20:17:22 +0800

    first commit

--- LeetCode/101_isSymmetric.py
+++ LeetCode/101_isSymmetric.py
@@ -27,32 +27,6 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
-#递归
-# class Solution(object):
-#     def isSymmetrical(self, pRoot):
-#         """
-#         :type root: TreeNode
-#         :rtype: bool
-#         """
-#         if pRoot == None:
-#             return True
-        
-#         return self.checkTwoTree(pRoot.left,pRoot.right)
-    
-#     def checkTwoTree(self, leftTree, rightTree):
-#         if leftTree==None and rightTree==None:
-#             return True
-#         if leftTree!=None and rightTree==None:
-#             return False
-#         if leftTree==None and rightTree!=None:
-#             return False
-#         if leftTree.val != rightTree.val:
-#             return False
-#         left = self.checkTwoTree(leftTree.left,rightTree.right)
-#         right = self.checkTwoTree(leftTree.right,rightTree.left)
-        
-#         return left and right
-#迭代
 class Solution(object):
     def isSymmetrical(self, pRoot):
         if not pRoot:
@@ -71,9 +45,6 @@ class Solution(object):
             node_list.append(symmetricRight.right)
             node_list.append(symmetricLeft.right)
             node_list.append(symmetricRight.left)
-            
         return True
 
-
-
-
+        
